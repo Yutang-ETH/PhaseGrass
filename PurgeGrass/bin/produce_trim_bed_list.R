ceaf4bd90dedca20bd33d45d726892925f29ae8c
Yutang-ETH <84848653+Yutang-ETH@users.noreply.github.com>
2024-08-14 10:06:49 +0200

    Delete PurgeGrass/bin directory

--- PurgeGrass/bin/produce_trim_bed_list.R
+++
@@ -1,9 +0,0 @@
-# produce trim files
-
-source("bin/cb_function.R")
-
-args = commandArgs(trailingOnly=TRUE)
-
-mycoords <- read.table("ps.coords", header = F, stringsAsFactors = F)
-
-Coords_and_haplotype_boundary_bed(coords = mycoords)
