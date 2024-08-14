0f0bd04fe1a27deae1e468a27188eba25a000fc8
Yutang-ETH <84848653+Yutang-ETH@users.noreply.github.com>
2024-08-14 10:08:40 +0200

    Update PURGEHAP.sh

--- PurgeGrass/PURGEHAP.sh
+++ PurgeGrass/PURGEHAP.sh
@@ -5,10 +5,10 @@
 # 14.04.2023
 
 # step 1, align ONT long reads to the assembly
-# minimap2 -t 60 -I 5G -K 5G -ax map-hifi sikem.asm.p_ctg.fasta ../all_hifi.fastq.gz --secondary=no | samtools sort -m 3G -@ 20 -o aligned.bam
+minimap2 -t 60 -I 5G -K 5G -ax map-hifi sikem.asm.p_ctg.fasta ../all_hifi.fastq.gz --secondary=no | samtools sort -m 3G -@ 20 -o aligned.bam
 
 # step 2, calculate read depth coverage 
-# purge_haplotigs hist -b aligned.bam -g sikem.asm.p_ctg.fasta -t 48
+purge_haplotigs hist -b aligned.bam -g sikem.asm.p_ctg.fasta -t 48
 
 # step 3, flag haplotigs
 purge_haplotigs cov -i aligned.bam.gencov -l 5 -m 37 -h 180 -j 101 -s 80
